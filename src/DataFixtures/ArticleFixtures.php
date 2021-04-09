1999af0c2b0ad14ff9bf7df00d4231f340cba42c
mllxmegan <mbarcelona11@hotmail.fr>
2021-04-09 11:11:13 +0200

    fonctionnel

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
+use App\DataFixtures\Faker;
+
+class ArticleFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = \Faker\Factory::create('fr_FR');
+
+        // Creer 3 catagories fakées
+        for($i = 1; $i <= 3; $i++){
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                     ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+
+            //Créer entre 4 et 6 articles
+            for($j = 1; $j <= mt_rand(4,6); $j++){
+                $article = new Article();
+
+                $content = '<p>' . join($faker->paragraphs(5), '</p><p>') . '</p>';
+
+                $article->setTitle($faker->sentence())
+                        ->setContent($content)
+                        ->setImage($faker->imageUrl())
+                        ->setCreatedAt($faker->dateTimeBetween('-6 months'))
+                        ->setCategory($category);
+    
+                $manager->persist($article);
+
+                // On donne des commentaires à l'<article></article>
+                for($k = 1; $k <= mt_rand(4,10); $k++){
+                    $comment = new Comment();
+
+                    $content = '<p>' . join($faker->paragraphs(2), '</p><p>') . '</p>';
+                    
+                    $now = new \DateTime();
+                    $days = $now->diff($article->getCreatedAt())->days;           
+
+                    $comment->setAuthor($faker->name)
+                            ->setContent($content)
+                            ->setCreatedAt($faker->dateTimeBetween('-' . $days . 'days'))
+                            ->setArticle($article);
+
+                    $manager->persist($comment);
+                }
+            }
+
+        }
+
+        
+        $manager->flush();
+    }
+}
